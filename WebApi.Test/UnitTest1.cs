fd26aa0d0818addd424c22b612ab30af3ebd9c1d
unknown <skose@it-myas.ilab.com.tr>
2016-04-21 17:35:36 +0300

    initial commit

---
+++ WebApi.Test/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using WebApi.Controllers;
+
+namespace WebApi.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new RsvpController();
+
+            var get = controller.GetAttendees();
+            Assert.IsTrue(get.Count > 0);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var controller = new RsvpController();
+            var response = new Models.GuestResponse() { Email = "abc@cde.com", Name = "Test Name", WillAttend = false };
+            controller.HebeleHubele(response);
+            var get = controller.GetAttendees();
+            Assert.IsTrue(!get.Contains(response));
+            //response.WillAttend = true;
+            //controller.HebeleHubele(response);
+            //get = controller.GetAttendees();
+            //Assert.IsTrue(get.Contains(response));
+
+        }
+    }
+}
---
+++ WebApi/Controllers/RsvpController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class RsvpController : ApiController
+    {
+        [HttpGet]
+        public List<GuestResponse> GetAttendees()
+        {
+            return Repository.Responses.Where(x => x.WillAttend == true).ToList();
+        }
+
+        [HttpPost]
+        public void HebeleHubele([FromBody] GuestResponse response)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Add(response);
+            }
+        }
+    }
+}
